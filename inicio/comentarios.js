154386e94d802ad7466aff9444b2574b1ab22062
alexADRV <verardo.alex@escola.pr.gov.br>
2025-04-15 22:26:54 -0300

    inicio com snetencas, blocos e dados

---
+++ inicio/comentarios.js
@@ -0,0 +1,8 @@
+// //console.log("passo #03"); o console é utilizado para mostrar algo no computador
+console.log("passo #01");
+console.log("passo #02");
+
+/* o console é
+utilizado para 
+mostrar algo na tela 
+do computador */
---
+++ inicio/desafio.bloco_sentenca.js
@@ -0,0 +1,19 @@
+// desafio: criar dois blocos de código o primeiro deve imprimir na tela os numeros pares de 0 a 10 e o segundo bloco deve imprimir na tela os impares.
+{
+  console.log("pares")
+  console.log("0");
+  console.log("2");
+  console.log("4");
+  console.log("6");
+  console.log("8");
+  console.log("10");
+}
+
+{
+  console.log("impares")
+  console.log("1");
+  console.log("3");
+  console.log("5");
+  console.log("7");
+  console.log("9");
+}
