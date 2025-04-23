f886724638cdf93cef064c20614a80028bcdb3e1
alexADRV <verardo.alex@escola.pr.gov.br>
2025-04-22 22:25:41 -0300

    desafios areas e troca

---
+++ inicio/desafio_area.js
@@ -0,0 +1,7 @@
+const PI = 3.141592;
+const raio = 10;
+let areaCirc = PI * raio * raio;
+
+// PI * raio 10²
+
+console.log("O valor da circunferência é: " + areaCirc);
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,13 @@
+// desafio: criar um programa que altere o valor de duas variaveis.
+// A primeira variavel tera o valor 94 e a segunda tera o valor 7.
+// imprimir os valores das variaveis na tela.
+
+var b = 94;
+var a = 7;
+
+var temp = b;
+b = a; // 7
+a = temp; // 94
+
+console.log(b);
+console.log(a);
