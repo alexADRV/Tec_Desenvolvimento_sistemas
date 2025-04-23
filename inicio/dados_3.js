461953c04be36a275ac13e59e85b3600d01c0c50
alexADRV <verardo.alex@escola.pr.gov.br>
2025-04-22 21:00:33 -0300

    lidando com dados(var, let, const)

---
+++ inicio/dados_3.js
@@ -0,0 +1,15 @@
+let idade = 16;
+console.log(typeof 16);
+console.log(typeof idade);
+
+let salario = 1512.50;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
+
+let nome = "Alex";
+console.log(typeof nome);
+
+console.log(1 + 1);
+console.log("1"+ 1);
