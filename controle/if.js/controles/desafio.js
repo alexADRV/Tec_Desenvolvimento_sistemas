3226df5e0e038027c1f9129a0930380c611cf5f4
alexADRV <verardo.alex@escola.pr.gov.br>
2025-05-13 22:21:15 -0300

    desafio

---
+++ controle/if.js/controles/desafio.js
@@ -0,0 +1,21 @@
+let numero = 1;
+if (numero > 0) {
+    console.log("o numero é positivo")
+}
+
+let nota = 85;
+if (nota > 60) {
+    console.log (" aprovado ");
+} else {
+    console.log("reprovado");
+}
+
+let idade = 20;
+if (idade <= 12) {
+    console.log("criança");
+} else if (idade <= 18) {
+    console.log("adolescente");
+} else {
+    console.log("adulto");
+}
+
