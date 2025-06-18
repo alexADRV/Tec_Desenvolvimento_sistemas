f4039175806dd296ec0031b5152f30320da4e025
alexADRV <verardo.alex@escola.pr.gov.br>
2025-06-17 21:11:16 -0300

    objetos em java

---
+++ objetos.js/exemplo.js
@@ -0,0 +1,25 @@
+console.log(aluno.nome)
+console.log(aluno("idade"))
+
+aluno.matricula = "2020A"
+aluno.idade = 18;
+
+delete aluno.curso;
+
+const carro ={
+    modelo "civic",
+    ano: 2020,
+    ligar: function () {
+        console.log("carro ligado")
+    }
+};
+
+carro.ligar(); // executar o métodos
+
+const produtos ={
+    {nome:"notebook", preco: 3500},
+    {nome:"mouse", preco: 80},
+    {nome:"teclado", preco: 150},
+}
+
+console.log(produto[1] nome )
